c8dac4c3d938be2ac0366efc943c7511c3978ec5
Rick Pospisil <rick.posp@gmail.com>
2024-03-17 19:16:46 -0500

    Adding code for combat
    Co-authored-by: bleeep11 <bleeep11@users.noreply.github.com>

--- main.py
+++ main.py
@@ -9,6 +9,81 @@
 # music
 # npc
 
+# combat gameplay
+# turn based
+# A little randomness (critical hits)
+# win by killing monster (health is zero)
+# Different moves available: attack, block, ranged attack, etc
+# inventory could take up a turn
+# God cheese
+
+
+# Combat
+# *companions
+# *three atacks per turn
+# potions
+#  * player inventory
+#  * option during combat
+# properties of your character factor in
+# AI???
+# God cheese
+
+class CombatController:
+
+    def __init__(self, player, enemy):
+        self.player = player
+        self.enemy = enemy
+
+    def enter_combat_loop(self):
+        while(self.enemy.alive()):
+            user_input = input("A: attack  B: block  C: ranged attack")
+            player_move = self.process_user_input_for_move(user_input)
+            player_move.do()
+            enemy_move = self.get_enemy_move()
+            enemy_move.do()
+            pass
+
+    def process_user_input_for_move(self, user_input):
+        clean_user_input = user_input.lower()
+        match(clean_user_input):
+            case "a":
+                return CombatMove("attack", 10, 0)
+            case "b":
+                return CombatMove("block", 0, 9)
+            case "c":
+                return CombatMove("ranged attack", 3, 0)
+            case _:
+                return None
+            
+    def get_enemy_move(self):
+        return CombatMove("attack", 9, 0)
+
+
+class Enemy:
+    def __init__(self, health, type, name):
+        self.health = health
+        self.type = type
+        self.name = name
+
+    def alive(self):
+        return self.health > 0
+
+
+class CombatMove:
+
+    def __init__(self, name, damage, block):
+        self.name = name
+        self.damage = damage
+        self.block = block
+
+
+class Player:
+
+    def __init__(self):
+        self.health = 100
+        self.inventory = {}
+
+
 def print_inventory():
     inventory = game_state["inventory"]
     for item in inventory:
